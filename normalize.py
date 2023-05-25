54b8d072652317d5c247b04a829cb1bb0e852074
Vinuyan Sivakolunthu <vinuyan.sivakolunthu@dawsoncollege.qc.ca>
2023-05-25 19:08:27 +0000

    updated normalize.py to be able to specify output dir

--- normalize.py
+++ normalize.py
@@ -1,13 +1,23 @@
 import nibabel as nib
-import numpy as np
 import click as ck
 import torchio as tio
 from pathlib import Path
+import os
 
 
 @ck.command()
 @ck.option("--input", "-i", type=ck.Path(), help="Input dir to normalize")
-def main(input):
+@ck.option("--output", "-o", type=ck.Path(), help="Output directory. Where all the normalized data will be saved")
+def main(input, output):
+    """
+    Normalize NIfTI images in a directory.
+
+    Args:
+        input (str): Input directory path.
+        output(str): Output directory path
+    Returns:
+        None
+    """
     dir_path = Path(input)
     for file_path in dir_path.iterdir():
         img = nib.load(file_path)
@@ -15,7 +25,7 @@ def main(input):
         RI_norm = tio.RescaleIntensity(out_min_max=(0, 1))
         normed_img = RI_norm(img2)
         neuro_img = nib.nifti1.Nifti1Image(normed_img.numpy().squeeze(), img.affine)
-        nib.save(neuro_img, f"norm-{file_path.name}")
+        nib.save(neuro_img, os.path.join(output, f"norm-{file_path.name}"))
 
 
 if __name__ == "__main__":
